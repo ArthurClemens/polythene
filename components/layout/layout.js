453e53b0f5540d080087962693fb1644aaa10f2c
Arthur Clemens <arthurclemens@gmail.com>
2015-03-30 21:56:14 +0200

    add layout component

---
+++ components/layout/layout.js
@@ -0,0 +1,9 @@
+module.exports = (function() {
+    'use strict';
+
+    var url = './layout.css';
+    var p = require('../polythene/polythene');
+    p.addStyleUrl(url);
+    //p.addStyle('layout', require('./layout.jss'));
+
+}());
