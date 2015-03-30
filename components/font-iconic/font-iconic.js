00aca52ce60fdc90ef0d473a99c43fbce2173a36
Arthur Clemens <arthurclemens@gmail.com>
2015-03-30 21:54:53 +0200

    add iconic font component

---
+++ components/font-iconic/font-iconic.js
@@ -0,0 +1,8 @@
+module.exports = (function() {
+    'use strict';
+
+    var url = '../../bower_components/material-design-iconic-font/css/material-design-iconic-font.min.css';
+    var p = require('../polythene/polythene.js');
+    p.addStyleUrl(url);
+    
+}());
