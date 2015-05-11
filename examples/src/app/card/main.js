250640bbed542d07afbb504cb87ca79a2c26f32d
Arthur Clemens <arthurclemens@gmail.com>
2015-05-11 23:02:48 +0200

    Card work in progress

--- examples/src/app/card/main.js
+++ examples/src/app/card/main.js
@@ -7,6 +7,8 @@ define(function(require) {
         app,
         nav = require('nav'),
         github = require('github'),
+        titleLineText,
+        infoLineText,
         block,
         content;
 
@@ -14,39 +16,97 @@ define(function(require) {
     require('css!app-css');
     require('css!./main');
 
+    titleLineText = 'Title';
+    infoLineText = 'Subhead';
+
     block = {
         view: function(ctrl, args) {
-            return m('.p-block', {class: args.class || ''}, [
+            return m('.p-block', {
+                class: args.class || ''
+            }, [
                 m('.p-block-header', args.label),
-                m.component(card, args.card)
+                args.card ? m.component(card, args.card) : null,
+                args.cards ? args.cards.map(function(cardArgs) {
+                    return m.component(card, cardArgs);
+                }) : null,
             ]);
         }
     };
 
     content = [
         m.component(block, {
-            label: 'Normal',
-            card: {
+            label: 'Depth',
+            cards: [{
                 class: 'demo-card',
-                content: 'hello',
-            }
-        }),
-        m.component(block, {
-            label: 'Flat',
-            card: {
+                content: 'Normal',
+            }, {
                 class: 'demo-card',
-                content: 'hello',
+                content: 'Flat',
                 z: 0
+            }, {
+                class: 'demo-card',
+                content: 'Raised',
+                z: 2
+            }]
+        }),
+        m.component(block, {
+            label: 'Media',
+            card: {
+                class: 'demo-card',
+                media: m('img', {
+                    src: 'app/images/bg1.jpg'
+                }),
+                content: 'hello'
             }
         }),
         m.component(block, {
-            label: 'Raised',
+            label: 'Header with icon',
             card: {
                 class: 'demo-card',
-                content: 'hello',
-                z: 2
+                header: {
+                    title: titleLineText,
+                    info: infoLineText,
+                    icon: {
+                        type: 'large',
+                        class: 'avatar',
+                        src: 'app/list-tile/avatars/1.png'
+                    }
+                },
+                media: m('img', {
+                    src: 'app/images/bg1.jpg'
+                }),
+                content: 'hello'
             }
-        })
+        }),
+        m.component(block, {
+            label: 'Primary text',
+            card: {
+                class: 'demo-card',
+                primary: {
+                    title: 'Primary title',
+                    info: 'Subtitle'
+                },
+                media: m('img', {
+                    src: 'app/images/bg1.jpg'
+                }),
+                content: 'hello'
+            }
+        }),
+        m.component(block, {
+            label: 'Combined',
+            cards: [{
+                class: 'demo-card',
+                header: {
+                    title: titleLineText,
+                    info: infoLineText,
+                    icon: {
+                        type: 'large',
+                        class: 'avatar',
+                        src: 'app/list-tile/avatars/1.png'
+                    }
+                }
+            }]
+        }),
     ];
 
     app = {};
---
+++ lib/card/card.md
@@ -0,0 +1,13 @@
+
+Parameters:
+
+content
+media
+header
+	title
+	info
+	icon
+primary
+	title
+	info
+
