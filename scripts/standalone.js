f49aea599bd4e21d92684b553cca449c982669b4
Arthur Clemens <arthurclemens@gmail.com>
2016-02-13 17:51:23 +0100

    standalone: add utils and fastclick

--- scripts/standalone.js
+++ scripts/standalone.js
@@ -4,6 +4,8 @@ const sh = require('shelljs');
 
 const folders = ['base-button', 'button', 'card', 'checkbox', 'common', 'config', 'dialog', 'element', 'fab', 'font-roboto', 'header-panel', 'icon', 'icon-button', 'layout', 'list', 'list-tile', 'menu', 'notification', 'polythene', 'radio-button', 'ripple', 'search', 'selection-control', 'shadow', 'slider', 'spinner', 'svg', 'switch', 'tabs', 'textfield', 'theme', 'toolbar'];
 
+const commonUtils = ['easing', 'mixin', 'multiple', 'no-tap-delay', 'object.assign', 'scroll-to', 'styler', 'timer', 'transition-event', 'transition', 'validation-helper', 'webfontloader'];
+
 const noRequires = {
     'common': 1,
     'font-roboto': 1,
@@ -16,9 +18,13 @@ const formatRequire = (path) => (`require('${path}');`);
 
 const requires = (formatFn) => {
     formatFn = formatFn || ((str) => (str));
-    return [formatFn('mithril')].concat(folders.filter(filterNoRequires).map((folder) => {
+    const components = [formatFn('mithril')].concat(folders.filter(filterNoRequires).map((folder) => {
         return formatFn(`polythene/${folder}/${folder}`);
     }));
+    const utils = [formatFn('fastclick')].concat(commonUtils.map((util) => {
+        return formatFn(`polythene/common/${util}`);
+    }));
+    return components.concat(utils);
 };
 
 const writeRequires = (callback) => {
