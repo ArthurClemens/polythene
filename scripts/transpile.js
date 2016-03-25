4982abf7e1ce4458ba0ee80a375b558195768087
Arthur Clemens <arthurclemens@gmail.com>
2016-03-25 15:05:24 +0100

    rename files: .es6.js to .es6

--- scripts/transpile.js
+++ scripts/transpile.js
@@ -9,9 +9,9 @@ watch ({
     persistent: !(process.argv[2] === 'once'),
     what: process.argv[3],
     ignore: (process.argv[4] && process.argv[4] !== 'null') ? process.argv[4] : null,
-    extension: '.es6.js',
+    extension: '.es6',
     createOutPath: function(inPath) {
-        return inPath.replace(/es6.js$/, 'js');
+        return inPath.replace(/es6$/, 'js');
     },
     transform: function(inPath, outPath) {
         return [
