d9fce608f251d9d2710a3aaa26f64c94cdc7df89
Arthur Clemens <arthurclemens@gmail.com>
2017-07-31 21:51:27 +0200

    Improve Search tests

--- packages/test-search/dist/test-search.mjs
+++ packages/test-search/dist/test-search.mjs
@@ -297,10 +297,10 @@ var _extends$1 = Object.assign || function (target) { for (var i = 1; i < argume
 
 function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
-var iconSearchSVG = "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\"><path d=\"M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z\"/></svg>";
-var iconBackSVG = "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\"><path d=\"M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z\"/></svg>";
-var iconClearSVG = "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\"><path d=\"M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z\"/></svg>";
-var iconMicSVG = "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\"><path d=\"M12 14c1.66 0 2.99-1.34 2.99-3L15 5c0-1.66-1.34-3-3-3S9 3.34 9 5v6c0 1.66 1.34 3 3 3zm5.3-3c0 3-2.54 5.1-5.3 5.1S6.7 14 6.7 11H5c0 3.41 2.72 6.23 6 6.72V21h2v-3.28c3.28-.48 6-3.3 6-6.72h-1.7z\"/></svg>";
+var iconSearchSVG = "<svg width=\"24\" height=\"24\" viewBox=\"0 0 24 24\"><path d=\"M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z\"/></svg>";
+var iconBackSVG = "<svg width=\"24\" height=\"24\" viewBox=\"0 0 24 24\"><path d=\"M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z\"/></svg>";
+var iconClearSVG = "<svg width=\"24\" height=\"24\" viewBox=\"0 0 24 24\"><path d=\"M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z\"/></svg>";
+var iconMicSVG = "<svg width=\"24\" height=\"24\" viewBox=\"0 0 24 24\"><path d=\"M12 14c1.66 0 2.99-1.34 2.99-3L15 5c0-1.66-1.34-3-3-3S9 3.34 9 5v6c0 1.66 1.34 3 3 3zm5.3-3c0 3-2.54 5.1-5.3 5.1S6.7 14 6.7 11H5c0 3.41 2.72 6.23 6 6.72V21h2v-3.28c3.28-.48 6-3.3 6-6.72h-1.7z\"/></svg>";
 
 var searchField = (function (_ref) {
   var h = _ref.renderer,
@@ -361,38 +361,40 @@ var searchField = (function (_ref) {
 
   return {
     oninit: function oninit(vnode) {
-      var inputEl = stream();
       var value = stream("");
+      var focus = stream(false);
+
       var clear = function clear() {
-        inputEl().value = "";
-        value("");
-        setTimeout(function () {
-          return inputEl().focus();
-        }, 0);
+        return value(""), focus(true);
       };
       var leave = function leave() {
-        inputEl().value = "";
-        value("");
+        return value("");
       };
+
       vnode.state = {
-        inputEl: inputEl,
         value: value,
+        focus: focus,
         clear: clear,
         leave: leave,
-        redrawOnUpdate: stream.merge([inputEl, value])
+        redrawOnUpdate: stream.merge([value]) // for React
       };
     },
     view: function view(_ref4) {
       var state = _ref4.state,
           attrs = _ref4.attrs;
 
+      var value = state.value();
+      var focus = state.focus();
       return h("div", [h(Search$$1, _extends$1({}, {
         textfield: {
           label: "Search",
           onChange: function onChange(_ref5) {
-            var el = _ref5.el;
-            return state.inputEl(el);
+            var value = _ref5.value,
+                focus = _ref5.focus;
+            return state.value(value), state.focus(focus);
           },
+          value: value,
+          focus: focus,
           key: "input"
         },
         buttons: {
@@ -4023,22 +4025,22 @@ function _inherits(subClass, superClass) { if (typeof superClass !== "function" 
 
 var iconSearch = react.createElement(
   "svg",
-  { xmlns: "http://www.w3.org/2000/svg", width: "24", height: "24", viewBox: "0 0 24 24" },
+  { width: "24", height: "24", viewBox: "0 0 24 24" },
   react.createElement("path", { d: "M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z" })
 );
 var iconBack = react.createElement(
   "svg",
-  { xmlns: "http://www.w3.org/2000/svg", width: "24", height: "24", viewBox: "0 0 24 24" },
+  { width: "24", height: "24", viewBox: "0 0 24 24" },
   react.createElement("path", { d: "M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z" })
 );
 var iconClear = react.createElement(
   "svg",
-  { xmlns: "http://www.w3.org/2000/svg", width: "24", height: "24", viewBox: "0 0 24 24" },
+  { width: "24", height: "24", viewBox: "0 0 24 24" },
   react.createElement("path", { d: "M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z" })
 );
 var iconMic = react.createElement(
   "svg",
-  { xmlns: "http://www.w3.org/2000/svg", width: "24", height: "24", viewBox: "0 0 24 24" },
+  { width: "24", height: "24", viewBox: "0 0 24 24" },
   react.createElement("path", { d: "M12 14c1.66 0 2.99-1.34 2.99-3L15 5c0-1.66-1.34-3-3-3S9 3.34 9 5v6c0 1.66 1.34 3 3 3zm5.3-3c0 3-2.54 5.1-5.3 5.1S6.7 14 6.7 11H5c0 3.41 2.72 6.23 6 6.72V21h2v-3.28c3.28-.48 6-3.3 6-6.72h-1.7z" })
 );
 
@@ -4094,7 +4096,7 @@ var searchField$1 = (function (_ref) {
 
       _this.state = {
         value: "",
-        fieldState: {}
+        focus: false
       };
       _this.clear = _this.clear.bind(_this);
       _this.leave = _this.leave.bind(_this);
@@ -4104,30 +4106,33 @@ var searchField$1 = (function (_ref) {
     _createClass(_class, [{
       key: "clear",
       value: function clear() {
-        var _this2 = this;
-
-        this.state.fieldState.el.value = "";
-        this.setState({ value: "" }, function () {
-          _this2.state.fieldState.el.focus();
+        this.setState({
+          value: "",
+          focus: true
         });
       }
     }, {
       key: "leave",
       value: function leave() {
-        this.state.fieldState.el.value = "";
         this.setState({ value: "" });
       }
     }, {
       key: "render",
       value: function render() {
-        var _this3 = this;
+        var _this2 = this;
 
+        var value = this.state.value;
+        var focus = this.state.focus;
         return react.createElement(Search$$1, {
           textfield: {
             label: "Search",
-            onChange: function onChange(fieldState) {
-              return _this3.setState({ fieldState: fieldState });
+            onChange: function onChange(_ref4) {
+              var value = _ref4.value,
+                  focus = _ref4.focus;
+              return _this2.setState({ value: value, focus: focus });
             },
+            value: value,
+            focus: focus,
             key: "input"
           },
           buttons: {
