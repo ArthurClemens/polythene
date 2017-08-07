606982a7eac74f452e3bef8b240d314a5f889a21
Arthur Clemens <arthurclemens@gmail.com>
2017-08-07 18:47:16 +0200

    Remove redundant markup from SVGs

--- packages/polythene-core-tabs/src/theme/index.js
+++ packages/polythene-core-tabs/src/theme/index.js
@@ -12,5 +12,5 @@ export const customTheme = (customSelector, customVars) =>
 
 styler.generateStyles([selector], vars, fns);
 
-export const arrowBackward = "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\"><path d=\"M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z\"/></svg>";
-export const arrowForward = "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\"><path d=\"M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z\"/></svg>";
+export const arrowBackward = "<svg width=\"24\" height=\"24\" viewBox=\"0 0 24 24\"><path d=\"M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z\"/></svg>";
+export const arrowForward = "<svg width=\"24\" height=\"24\" viewBox=\"0 0 24 24\"><path d=\"M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z\"/></svg>";
--- packages/tests-render-mithril/src/page.js
+++ packages/tests-render-mithril/src/page.js
@@ -2,7 +2,7 @@ import m from "mithril";
 import { rules as css } from "./styles";
 import { renderer as h, Dialog, IconButton, Toolbar, Notification, Snackbar } from "polythene-mithril";
 
-const iconBack = m.trust("<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\"><path d=\"M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z\"/></svg>");
+const iconBack = m.trust("<svg width=\"24\" height=\"24\" viewBox=\"0 0 24 24\"><path d=\"M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z\"/></svg>");
 
 const navBar = (name, previous) =>
   h(css.headerRow, h(Toolbar, {
