94194bab41ff506702ab35e6b6f89fb2945f9f54
Arthur Clemens <arthurclemens@gmail.com>
2017-08-09 17:08:04 +0200

    Dev: copy demos script

---
+++ scripts/copyToDemos.js
@@ -0,0 +1,14 @@
+/* global process */
+const shell = require("shelljs");
+
+const baseDir = process.cwd();
+
+const buildAndCopy = which => {
+  shell.cd(`packages/tests-render-${which}/`);
+  shell.exec("yarn run build");
+  shell.cd(baseDir);
+  shell.cp("-R", `packages/tests-render-${which}/dist/*`, `../../polythene-demos/gh-pages/${which}`);
+};
+
+buildAndCopy("mithril");
+buildAndCopy("react");
