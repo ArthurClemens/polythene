9e1c3e3c45c96b12532bcaa91feb40389d79bcfc
Arthur Clemens <arthurclemens@gmail.com>
2017-08-13 14:54:06 +0200

    Standalone: add subscribe and unsubscribe

--- packages/polythene-react/standalone.js
+++ packages/polythene-react/standalone.js
@@ -1,4 +1,5 @@
 export * from "./index";
+export { subscribe, unsubscribe } from "polythene-core";
 import { addLayoutStyles } from "polythene-utilities";
 import { addTypography, addRoboto } from "polythene-style";
 import { addFastClick } from "polythene-fastclick";
