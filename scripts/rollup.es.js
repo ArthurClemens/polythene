4aa21c1ec453a1002bf06020e7e8def0a0cd161d
Arthur Clemens <arthurclemens@gmail.com>
2017-10-02 17:25:01 +0200

    Build script minor

--- scripts/rollup.es.js
+++ scripts/rollup.es.js
@@ -3,7 +3,7 @@
 Build to a module that has ES2015 module syntax but otherwise only syntax features that node supports
 https://github.com/rollup/rollup/wiki/jsnext:main
 */
-import { pkg, createConfig } from "./rollup.base.js";
+import { pkg, createConfig } from "./rollup.base";
 
 const includeDepencies = !!parseInt(process.env.DEPS, 10) || false; // Use `false` if you are creating a library, or if you are including external script in html
 
