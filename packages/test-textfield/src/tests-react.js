958be84aa3d44135a4ef2604df212248e8e0465c
Arthur Clemens <arthurclemens@gmail.com>
2017-11-18 16:18:23 +0100

    Restore tests

--- packages/test-textfield/src/tests-react.js
+++ packages/test-textfield/src/tests-react.js
@@ -16,44 +16,44 @@ const reactTests = ({ TextField, RaisedButton, renderer: h }) => { // eslint-dis
     >{test}</div>;
 
   return [
-    // {
-    //   section: "React JSX tests",
-    // },
-    // {
-    //   name: "Option: value (JSX)",
-    //   component: () =>
-    //     block([
-    //       <TextField
-    //         type="password"
-    //         defaultValue="123456"
-    //         key="a"
-    //       />,
-    //       <TextField
-    //         type="number"
-    //         defaultValue="123456"
-    //         key="b"
-    //       />,
-    //       <TextField
-    //         type="email"
-    //         defaultValue="a@b.com"
-    //         key="c"
-    //       />
-    //     ])
-    // },
-    // {
-    //   name: "Option: counter, floatingLabel (JSX)",
-    //   interactive: true,
-    //   component: () =>
-    //     block([
-    //       <TextField
-    //         label="Description"
-    //         floatingLabel
-    //         counter={15}
-    //         error="You have exceeded the maximum number of characters."
-    //         key="x"
-    //       />
-    //     ])
-    // },
+    {
+      section: "React JSX tests",
+    },
+    {
+      name: "Option: value (JSX)",
+      component: () =>
+        block([
+          <TextField
+            type="password"
+            defaultValue="123456"
+            key="a"
+          />,
+          <TextField
+            type="number"
+            defaultValue="123456"
+            key="b"
+          />,
+          <TextField
+            type="email"
+            defaultValue="a@b.com"
+            key="c"
+          />
+        ])
+    },
+    {
+      name: "Option: counter, floatingLabel (JSX)",
+      interactive: true,
+      component: () =>
+        block([
+          <TextField
+            label="Description"
+            floatingLabel
+            counter={15}
+            error="You have exceeded the maximum number of characters."
+            key="x"
+          />
+        ])
+    },
   ];
 };
 
