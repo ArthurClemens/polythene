01062369e172c06fda4403112810d32a5f70fed8
Arthur Clemens <arthurclemens@gmail.com>
2017-11-18 13:27:32 +0100

    Make Phenomic test similar to react setup

--- packages/tests-render-react-phenomic/src/App.js
+++ packages/tests-render-react-phenomic/src/App.js
@@ -1,10 +1,21 @@
 import React from "react";
-import { RaisedButton } from "polythene-react";
+import { RaisedButton, Dialog } from "polythene-react";
 import Layout from "./Layout";
 
 const App = () => (
   <Layout>
-    <RaisedButton label="Button" />
+    <RaisedButton
+      label="Open dialog"
+      events={{
+        onClick: () => Dialog.show({
+          /* note the Dialog component is below the other elements in the app */
+          title: "Hello",
+          body: "Click outside to close, or press ESCAPE",
+          backdrop: true
+        })
+      }}
+    />
+    <Dialog />
   </Layout>
 );
 
