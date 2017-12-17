c234c83fe995dd787fa2f6d59c6cce77264fd6bd
Arthur Clemens <arthurclemens@gmail.com>
2017-12-17 17:20:06 +0100

    FastClick: simplify so that it works on Android

--- packages/polythene-fastclick/src/fastclick.js
+++ packages/polythene-fastclick/src/fastclick.js
@@ -1,41 +1,12 @@
 import FastClick from "fastclick";
-import { isTouch, subscribe, isClient } from "polythene-core";
+import { isTouch, isClient } from "polythene-core";
 
-const THROTTLE_DELAY = 100;
-const REINIT_DELAY = THROTTLE_DELAY + 50;
 const layer = isClient
-  ? document.body
+  ? document.scrollingElement
   : { body: {} };
-
-let fastClick;
-let timeoutId;
-let enabled;
-
-const add = () => {
-  if (!enabled) {
-    fastClick = new FastClick(layer);
-    enabled = true;
-  }
-};
-
-const remove = () => {
-  if (enabled) {
-    fastClick.destroy();
-    enabled = false;
-  }
-};
-
-const handleScroll = () => {
-  remove();
-  if (timeoutId) {
-    clearTimeout(timeoutId);
-  }
-  timeoutId = setTimeout(add, REINIT_DELAY);
-};
 
 export const addFastClick = () => {
   if (isTouch && isClient) {
-    subscribe("scroll", handleScroll, THROTTLE_DELAY);
-    add();
+    new FastClick(layer);
   }
 };
