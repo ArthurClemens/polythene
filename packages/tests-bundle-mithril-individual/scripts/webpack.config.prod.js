5d0942548f33851a4146c295af5ea836fa985047
Arthur Clemens <arthurclemens@gmail.com>
2018-01-01 15:35:51 +0100

    Tests: update build scripts

--- packages/tests-bundle-mithril-individual/scripts/webpack.config.prod.js
+++ packages/tests-bundle-mithril-individual/scripts/webpack.config.prod.js
@@ -1,12 +1,15 @@
 
 const config = require("./webpack.config.js");
 const webpack = require("webpack");
+const UglifyWebpackPlugin = require("uglifyjs-webpack-plugin");
 const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 const CompressionPlugin = require("compression-webpack-plugin");
 
 config.plugins.push(new webpack.DefinePlugin({
   "process.env": { NODE_ENV: JSON.stringify("production") }
 }));
+
+config.plugins.push(new UglifyWebpackPlugin());
 
 config.plugins.push(new BundleAnalyzerPlugin({ analyzerMode: "static" }));
 config.plugins.push(new CompressionPlugin({
--- packages/tests-render-react/scripts/webpack.config.prod.js
+++ packages/tests-render-react/scripts/webpack.config.prod.js
@@ -2,7 +2,6 @@
 const config = require("./webpack.config.js");
 const webpack = require("webpack");
 const UglifyWebpackPlugin = require("uglifyjs-webpack-plugin");
-const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 const CompressionPlugin = require("compression-webpack-plugin");
 
 config.plugins.push(new webpack.DefinePlugin({
@@ -11,7 +10,6 @@ config.plugins.push(new webpack.DefinePlugin({
 
 config.plugins.push(new UglifyWebpackPlugin());
 
-config.plugins.push(new BundleAnalyzerPlugin({ analyzerMode: "static" }));
 config.plugins.push(new CompressionPlugin({
   asset: "[path].gz[query]",
   algorithm: "gzip",
