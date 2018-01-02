082efd2a56fd1b543a5ed1568fe2be3a0d100f5b
Arthur Clemens <arthurclemens@gmail.com>
2018-01-02 12:18:33 +0100

    Standalone: remove FastClick

--- packages/polythene-react/scripts/standalone.js
+++ packages/polythene-react/scripts/standalone.js
@@ -1,8 +1,6 @@
 export * from "../index";
 export { subscribe, unsubscribe } from "polythene-core";
 import { addTypography, addLayoutStyles } from "polythene-css";
-import { addFastClick } from "polythene-fastclick";
 
 addTypography();
 addLayoutStyles();
-addFastClick();
