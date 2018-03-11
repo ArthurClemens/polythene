cfd6c3a189d834b8d7d8db0d2577033a35321961
Arthur Clemens <arthurclemens@gmail.com>
2018-03-11 16:03:17 +0100

    Minor

--- packages/polythene-core/src/deprecated.js
+++ packages/polythene-core/src/deprecated.js
@@ -1,3 +1,3 @@
 
 export const deprecation = (component, deprecatedOption, newOption) =>
-  console.warn(`${component}: option '${deprecatedOption}' is deprecated and will be removed in later versions. Use '${newOption}' instead.`);
+  console.warn(`${component}: option '${deprecatedOption}' is deprecated and will be removed in later versions. Use '${newOption}' instead.`); // eslint-disable-line no-console
