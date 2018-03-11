e7bccc95634f0ea8ab5d9b098122e59a580b36b9
Arthur Clemens <arthurclemens@gmail.com>
2018-03-11 17:23:58 +0100

    Webpack 3 test

---
+++ packages/tests-render-webpack-3/scripts/webpack.config.prod.js
@@ -0,0 +1,23 @@
+
+const config = require("./webpack.config.js");
+const webpack = require("webpack");
+const UglifyWebpackPlugin = require("uglifyjs-webpack-plugin");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+const CompressionPlugin = require("compression-webpack-plugin");
+
+config.plugins.push(new webpack.DefinePlugin({
+  "process.env": { NODE_ENV: JSON.stringify("production") }
+}));
+
+config.plugins.push(new UglifyWebpackPlugin());
+
+config.plugins.push(new BundleAnalyzerPlugin({ analyzerMode: "static" }));
+config.plugins.push(new CompressionPlugin({
+  asset: "[path].gz[query]",
+  algorithm: "gzip",
+  test: /\.js$|\.css$|\.html$/,
+  threshold: 10240,
+  minRatio: 0
+}));
+
+module.exports = config;
