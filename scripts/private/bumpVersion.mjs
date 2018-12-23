6819241608cd01a1344991ffc0cde44ab1b56a89
Arthur Clemens <arthurclemens@gmail.com>
2018-12-23 16:12:42 +0100

    Bump version script

---
+++ scripts/private/bumpVersion.mjs
@@ -0,0 +1,51 @@
+/* global process */
+import child_process from "child_process";
+import fs from "fs";
+import glob from "fast-glob";
+import parseArgs from "minimist";
+import shell from "shelljs";
+
+const baseDir = process.cwd();
+const PACKAGE_FILE_PATTERN = "./packages/*/package.json";
+
+const argv = parseArgs(process.argv.slice(2));
+const isTestRun = !!argv["dry-run"];
+const newVersion = argv["version"]
+
+const update = filename => {
+  return new Promise((resolve, reject) => {
+    const fileContents = fs.readFileSync(filename, "utf8");
+    const data = JSON.parse(fileContents);
+    const name = data.name;
+    console.log("------------------------------");
+    console.log(`Package: ${name}`); // eslint-disable-line no-console
+    if (data.version !== newVersion) {
+      if (isTestRun) {
+        console.log("DRY RUN", `"change version to": "${newVersion}"`); // eslint-disable-line no-console
+      } else {
+        shell.sed("-i", `"version": ".*"`, `"version": "${newVersion}"`, filename);
+        shell.sed("-i", `"polythene-(.*)": ".*?"`, `"polythene-$1": "^${newVersion}"`, filename);
+      }
+      resolve(filename);
+    } else {
+      console.log(`${name} is already at ${newVersion}`); // eslint-disable-line no-console
+      reject(filename);
+    }
+  });
+};
+
+async function collectAndUpdate() {
+  const readStream = glob.stream([PACKAGE_FILE_PATTERN, "!./node_modules/**/*"]);
+  let resolved = [];
+  let rejected = [];
+  for await (const path of readStream) {
+    await update(path).then(
+      path => resolved.push(path),
+      path => rejected.push(path),
+    );
+  }
+  console.log(`Updated: ${resolved.length}`, resolved.length > 0 ? resolved : "");
+  console.log(`Not updated: ${rejected.length}`, rejected.length > 0 ? rejected : "");
+}
+
+collectAndUpdate();
---
+++ scripts/private/publish.mjs
@@ -0,0 +1,69 @@
+/* global process */
+import child_process from "child_process";
+import fs from "fs";
+import glob from "fast-glob";
+import parseArgs from "minimist";
+import path from "path";
+import shell from "shelljs";
+
+const baseDir = process.cwd();
+const PACKAGE_FILE_PATTERN = "./packages/*/package.json";
+
+const argv = parseArgs(process.argv.slice(2));
+const isTestRun = !!argv["dry-run"];
+const newVersion = argv["version"]
+
+shell.exec("npm whoami", { silent: true }, (code, stdout, stderr) => {
+  if (stderr) {
+    console.log("log in to npm first.");
+  }
+});
+
+const maybePublish = filename => {
+  return new Promise((resolve, reject) => {
+    const fileContents = fs.readFileSync(filename, "utf8");
+    const data = JSON.parse(fileContents);
+    const name = data.name;
+    console.log("------------------------------");
+    console.log(`Package: ${name}`); // eslint-disable-line no-console
+    if (data.private) {
+      console.log("Private package: won't publish");
+      reject(filename);
+      return;
+    }
+    shell.exec(`npm view ${name} version`, (code, npmVersion) => {
+      const version = npmVersion.trim();
+      console.log(`npm for ${name} is at ${version}`); // eslint-disable-line no-console
+      if (version !== newVersion) {
+        if (isTestRun) {
+          console.log("DRY RUN", `Publish: ${name}`); // eslint-disable-line no-console
+          resolve(filename)
+        } else {
+          shell.cd(path.dirname(filename));
+          shell.exec("npm publish");
+          shell.cd(baseDir);
+          child_process.execSync("sleep 1");
+          resolve(filename);
+        }
+      } else {
+        reject(filename);
+      }
+    });
+  });
+};
+
+async function collectAndPublish() {
+  const readStream = glob.stream([PACKAGE_FILE_PATTERN, "!./node_modules/**/*"]);
+  let resolved = [];
+  let rejected = [];
+  for await (const path of readStream) {
+    await maybePublish(path).then(
+      path => resolved.push(path),
+      path => rejected.push(path),
+    );
+  }
+  console.log(`Published: ${resolved.length}`, resolved.length > 0 ? resolved : "");
+  console.log(`Not published: ${rejected.length}`, rejected.length > 0 ? rejected : "");
+}
+
+collectAndPublish();
