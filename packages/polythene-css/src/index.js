67030dd18ceb50a501cb2cf462e79c318e5ecb40
Arthur Clemens <arthurclemens@gmail.com>
2019-03-10 22:31:29 +0100

    CSS-in-JS: Use intermediate variables to bypass Webpack's treeshaking

--- packages/polythene-css/src/index.js
+++ packages/polythene-css/src/index.js
@@ -1,34 +1,129 @@
-import * as BaseSpinnerCSS                   from "polythene-css-base-spinner";                     export { BaseSpinnerCSS };
-import * as ButtonCSS                        from "polythene-css-button";                           export { ButtonCSS };
-import * as ButtonGroupCSS                   from "polythene-css-button-group";                     export { ButtonGroupCSS };
-import * as CardCSS                          from "polythene-css-card";                             export { CardCSS };
-import * as CheckboxCSS                      from "polythene-css-checkbox";                         export { CheckboxCSS };
-import * as DialogCSS                        from "polythene-css-dialog";                           export { DialogCSS };
-import * as DialogPaneCSS                    from "polythene-css-dialog-pane";                      export { DialogPaneCSS };
-import * as DrawerCSS                        from "polythene-css-drawer";                           export { DrawerCSS };
-import * as FABCSS                           from "polythene-css-fab";                              export { FABCSS };
-import * as IconCSS                          from "polythene-css-icon";                             export { IconCSS };
-import * as IconButtonCSS                    from "polythene-css-icon-button";                      export { IconButtonCSS };
-import * as IOSSpinnerCSS                    from "polythene-css-ios-spinner";                      export { IOSSpinnerCSS };
-import * as ListCSS                          from "polythene-css-list";                             export { ListCSS };
-import * as ListTileCSS                      from "polythene-css-list-tile";                        export { ListTileCSS };
-import * as MaterialDesignProgressSpinnerCSS from "polythene-css-material-design-progress-spinner"; export { MaterialDesignProgressSpinnerCSS };
-import * as MaterialDesignSpinnerCSS         from "polythene-css-material-design-spinner";          export { MaterialDesignSpinnerCSS };
-import * as MenuCSS                          from "polythene-css-menu";                             export { MenuCSS };
-import * as NotificationCSS                  from "polythene-css-notification";                     export { NotificationCSS };
-import * as RadioButtonCSS                   from "polythene-css-radio-button";                     export { RadioButtonCSS };
-import * as RippleCSS                        from "polythene-css-ripple";                           export { RippleCSS };
-import * as SearchCSS                        from "polythene-css-search";                           export { SearchCSS };
-import * as SelectionControlCSS              from "polythene-css-selection-control";                export { SelectionControlCSS };
-import * as ShadowCSS                        from "polythene-css-shadow";                           export { ShadowCSS };
-import * as SliderCSS                        from "polythene-css-slider";                           export { SliderCSS };
-import * as SnackbarCSS                      from "polythene-css-snackbar";                         export { SnackbarCSS };
-import * as SVGCSS                           from "polythene-css-svg";                              export { SVGCSS };
-import * as SwitchCSS                        from "polythene-css-switch";                           export { SwitchCSS };
-import * as TabsCSS                          from "polythene-css-tabs";                             export { TabsCSS };
-import * as TextFieldCSS                     from "polythene-css-textfield";                        export { TextFieldCSS };
-import * as ToolbarCSS                       from "polythene-css-toolbar";                          export { ToolbarCSS };
-import * as TypographyCSS                    from "polythene-css-typography";                       export { TypographyCSS };
+
+// Use intermediate variables to bypass Webpack's treeshaking
+
+import * as _BaseSpinnerCSS from "polythene-css-base-spinner";
+const BaseSpinnerCSS = _BaseSpinnerCSS;
+export { BaseSpinnerCSS };
+
+import * as _ButtonCSS from "polythene-css-button";
+const ButtonCSS = _ButtonCSS;
+export { ButtonCSS };
+
+import * as _ButtonGroupCSS from "polythene-css-button-group";
+const ButtonGroupCSS = _ButtonGroupCSS;
+export { ButtonGroupCSS };
+
+import * as _CardCSS from "polythene-css-card";
+const CardCSS = _CardCSS;
+export { CardCSS };
+
+import * as _CheckboxCSS from "polythene-css-checkbox";
+const CheckboxCSS = _CheckboxCSS;
+export { CheckboxCSS };
+
+import * as _DialogCSS from "polythene-css-dialog";
+const DialogCSS = _DialogCSS;
+export { DialogCSS };
+
+import * as _DialogPaneCSS from "polythene-css-dialog-pane";
+const DialogPaneCSS = _DialogPaneCSS;
+export { DialogPaneCSS };
+
+import * as _DrawerCSS from "polythene-css-drawer";
+const DrawerCSS = _DrawerCSS;
+export { DrawerCSS };
+
+import * as _FABCSS from "polythene-css-fab";
+const FABCSS = _FABCSS;
+export { FABCSS };
+
+import * as _IconCSS from "polythene-css-icon";
+const IconCSS = _IconCSS;
+export { IconCSS };
+
+import * as _IconButtonCSS from "polythene-css-icon-button";
+const IconButtonCSS = _IconButtonCSS;
+export { IconButtonCSS };
+
+import * as _IOSSpinnerCSS from "polythene-css-ios-spinner";
+const IOSSpinnerCSS = _IOSSpinnerCSS;
+export { IOSSpinnerCSS };
+
+import * as _ListCSS from "polythene-css-list";
+const ListCSS = _ListCSS;
+export { ListCSS };
+
+import * as _ListTileCSS from "polythene-css-list-tile";
+const ListTileCSS = _ListTileCSS;
+export { ListTileCSS };
+
+import * as _MaterialDesignProgressSpinnerCSS from "polythene-css-material-design-progress-spinner";
+const MaterialDesignProgressSpinnerCSS = _MaterialDesignProgressSpinnerCSS;
+export { MaterialDesignProgressSpinnerCSS };
+
+import * as _MaterialDesignSpinnerCSS from "polythene-css-material-design-spinner";
+const MaterialDesignSpinnerCSS = _MaterialDesignSpinnerCSS;
+export { MaterialDesignSpinnerCSS };
+
+import * as _MenuCSS from "polythene-css-menu";
+const MenuCSS = _MenuCSS;
+export { MenuCSS };
+
+import * as _NotificationCSS from "polythene-css-notification";
+const NotificationCSS = _NotificationCSS;
+export { NotificationCSS };
+
+import * as _RadioButtonCSS from "polythene-css-radio-button";
+const RadioButtonCSS = _RadioButtonCSS;
+export { RadioButtonCSS };
+
+import * as _RippleCSS from "polythene-css-ripple";
+const RippleCSS = _RippleCSS;
+export { RippleCSS };
+
+import * as _SearchCSS from "polythene-css-search";
+const SearchCSS = _SearchCSS;
+export { SearchCSS };
+
+import * as _SelectionControlCSS from "polythene-css-selection-control";
+const SelectionControlCSS = _SelectionControlCSS;
+export { SelectionControlCSS };
+
+import * as _ShadowCSS from "polythene-css-shadow";
+const ShadowCSS = _ShadowCSS;
+export { ShadowCSS };
+
+import * as _SliderCSS from "polythene-css-slider";
+const SliderCSS = _SliderCSS;
+export { SliderCSS };
+
+import * as _SnackbarCSS from "polythene-css-snackbar";
+const SnackbarCSS = _SnackbarCSS;
+export { SnackbarCSS };
+
+import * as _SVGCSS from "polythene-css-svg";
+const SVGCSS = _SVGCSS;
+export { SVGCSS };
+
+import * as _SwitchCSS from "polythene-css-switch";
+const SwitchCSS = _SwitchCSS;
+export { SwitchCSS };
+
+import * as _TabsCSS from "polythene-css-tabs";
+const TabsCSS = _TabsCSS;
+export { TabsCSS };
+
+import * as _TextFieldCSS from "polythene-css-textfield";
+const TextFieldCSS = _TextFieldCSS;
+export { TextFieldCSS };
+
+import * as _ToolbarCSS from "polythene-css-toolbar";
+const ToolbarCSS = _ToolbarCSS;
+export { ToolbarCSS };
+
+import * as _TypographyCSS from "polythene-css-typography";
+const TypographyCSS = _TypographyCSS;
+export { TypographyCSS };
 
 export { addTypography, addRoboto } from "polythene-css-typography";
 export { addLayoutStyles } from "polythene-core-css";
