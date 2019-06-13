e55582f292130dfba19458ff53a70f8c507113b0
Arthur Clemens <arthurclemens@gmail.com>
2019-06-13 18:50:47 +0200

    Add layout styles to polythene.css

--- scripts/private/buildComponentCSS.js
+++ scripts/private/buildComponentCSS.js
@@ -10,17 +10,18 @@ const build = function(dir) {
   shell.cd(baseDir);
 };
 
+const buildDir = dir => {
+  if (shell.test("-d", dir)) {
+    console.log(`Building dir ${dir}`); // eslint-disable-line no-console
+    build(dir);
+  } else {
+    console.log(`Directory ${dir} does not exist`); // eslint-disable-line no-console
+  }
+};
+
 shell.exec("lerna bootstrap");
 
 componentNames.forEach(function(name) {
   const componentCssDir = `packages/polythene-css-${name}`;
-
-  [componentCssDir].forEach(function(dir) {
-    if (shell.test("-d", dir)) {
-      console.log(`Building dir ${dir}`); // eslint-disable-line no-console
-      build(dir);
-    } else {
-      console.log(`Directory ${dir} does not exist`); // eslint-disable-line no-console
-    }
-  });
+  buildDir(componentCssDir);
 });
